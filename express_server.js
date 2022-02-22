fc91ad7f1e3f641cb1408d15d5429c2c864fd61d
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-21 18:49:11 -0800

    add bodyPraser, generateRandom shortURL, setup urls/new and post new urls

--- express_server.js
+++ express_server.js
@@ -1,22 +1,39 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  return Math.random().toString(36).replace("0.", "").substring(0, 5);
+}
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+//use ejs to render new pages
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (request, response) => {
   const templateVars = {
-    shortURL: request.params.shortURL,
+    shortURL: request.params.shortURL, //get shortURL, it is obj key
     longURL: urlDatabase[request.params.shortURL],
   };
   response.render("urls_show", templateVars);
@@ -34,6 +51,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// check if fetch will get result of set, answer is no. from diffent page
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
