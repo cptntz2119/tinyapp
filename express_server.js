973d67f86b37ab415202a55462db2cb4f37e685a
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-22 21:10:06 -0800

    on new branch, and introduce cookies got user login and logout options

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 function generateRandomString() {
   return Math.random().toString(36).replace("0.", "").substring(0, 6);
@@ -16,15 +18,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-//use ejs to render new pages
+//use ejs to render new pages, when add login update template
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-//route: GET
+//route: GET, update with user login by adding template
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 //route: ADD new url
@@ -37,6 +40,19 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randomShort}`);
 });
 
+//route: introduce cookies so that user login and log out is added
+app.post("/login", (req, res) => {
+  //let username = req.cookies.username;
+  res.cookie("username", req.body.username);
+  console.log("cookie usename is: ", req.body.username);
+  res.redirect("/urls");
+});
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  //console.log("cookie usename is: ", req.body.username);
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     res.redirect(urlDatabase[req.params.shortURL]);
@@ -47,8 +63,8 @@ app.get("/u/:shortURL", (req, res) => {
 
 //route: UPDATE
 app.post("/urls/:shortURL", (req, res) => {
-  console.log("route update", req.params);
-  console.log(req.body);
+  //console.log("route update", req.params);
+  //console.log(req.body);
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls/");
 });
